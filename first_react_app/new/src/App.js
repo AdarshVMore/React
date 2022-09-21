a6360279ff8166c5b8b985905ad3eb6ea6dd3a4e
AdarshVMore <skullcrushermore@gmail.com>
2022-09-21 16:32:37 +0530

    made some comments

--- first_react_app/new/src/App.js
+++ first_react_app/new/src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {Route , BrowserRouter} from 'react-router-dom'
+import './App.css'
+import './index.css';
+
 
 function App() {
+  // ================  RANDOM QUOTE GENERATOR
+
+  let i = 0
+  const quotes = ["hiii", "hello" , "there" , "new" , "quote" , "is" , "here"]
+  const [quote , newquote] = useState(quotes[0])
+  const [first , next] = useState(0)
+  var nextquote = quotes[i]
+
+  function nextQuote(){
+    var random = Math.floor(Math.random() * quotes.length)
+    newquote(quotes[random])
+
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h3 className='displayQuote'>{quote}</h3>
+      <button onClick={nextQuote}>Change</button>
+    </div>
     </div>
   );
-}
 
-export default App;
+
+  // ==================  TODO APP
+
+  // const [task, newTask] = useState("")
+  // const [todos, newtodo] = useState([])
+
+
+  // function addtask(event) {
+  //   event.preventDefault()
+  //   newtodo(oldtodos => {
+  //     newTask('')
+  //     return [...oldtodos, task]
+  //   })
+  // }
+  // function delete_todo() {
+    
+  // }
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <form onSubmit={addtask}>
+  //         <input type="text" value={task} onChange={e =>
+  //           newTask(e.target.value)} />
+  //         <button type='submit'>Add todo</button>
+  //       </form>
+  //     </div>
+  //     <ul>
+  //       {todos.map(todo =>
+  //         <div>
+  //           <li>{todo}</li>
+  //           <button onClick={delete_todo}>Delete</button>
+  //         </div>
+  //       )}
+  //     </ul>
+  //   </div>
+  // )
+
+
+
+
+  // ========================================> ROUTING
+  
+  // <BrowserRouter>
+  //   <link to='/hello-world' exact>go to hello world</link>
+  //   <Route path="/hello-world">
+  //     <h1>Hello world</h1>
+  //   </Route>
+  // </BrowserRouter> 
+
+
+
+  // ==========================================> PROPS
+
+
+// function Person (props) {
+//   return(
+//     <>
+//       <p>fname: {props.fname}</p>
+//       <p>lname: {props.lname}</p>
+//       <p>age: {props.age}</p>
+
+//     </>
+//   )
+// }
+
+
+//   return (
+//     <div className='App'>
+//       <Person fname={'Adarsh'} lname='More' age='19'/>
+//       <Person fname={'Adarsh'}/>
+//       <Person/>
+//     </div>
+//   );
+
+
+// =============================================>  USESTATE
+
+// MAKING A COUNTING APP
+
+  // const [old , next] = useState(0)
+
+  // function add(){
+  //   next(old+1)
+  // }
+  // function sub(){
+  //   next(old-1)
+  // }
+
+  // return(
+  //   <>
+  //     <button onClick={sub}>-</button>
+  //     <h1>{old}</h1>
+  //     <button onClick={add}>+</button>
+  //   </>
+  // ) 
+
+}
+}
+export default App
